3682433d1fe90108ce65b4c2a91e5494ebaac71e
brisarodriguez <brisarodriguez@localhost>
2023-09-13 14:16:35 -0500

    in progress

---
+++ src/studio2/PI.java
@@ -0,0 +1,16 @@
+package studio2;
+
+public class PI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double dartX =0; 
+		double dartY = 0; 
+		dartX = Math.random()*1; 
+		System.out.println(dartX);
+		dartY=Math.random()*1; 
+		System.out.println(dartY); 
+		
+	}
+
+}
